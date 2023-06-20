c8e062f3118224bb847d104e0b4ca5531cd9a216
Mariya Moiseeva <mash.dobarina@gmail.com>
2023-06-20 14:01:22 +0300

    Start project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1> Task Manager </h1>
+    <my-component> </my-component>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   MyComponent
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style >
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
+
+
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="root">
+       
+        
+    </div>
+</template>
+
+<script>
+
+export default ({
+    name: "MyComponent",
+
+    data() {
+        return {
+            
+        
+        }
+    },
+
+    computed: {
+      
+    },
+
+})
+</script>
