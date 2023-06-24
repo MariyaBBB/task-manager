06dc2711905f86a7b5b94841418d523ac39ae0d2
Mariya Moiseeva <mash.dobarina@gmail.com>
2023-06-24 22:45:24 +0300

    Добавлен компонент  Manager Task

---
+++ src/components/ManagerTask.vue
@@ -0,0 +1,106 @@
+<template>
+    <div id="task" >
+        <div 
+            class="container"
+            v-for="task in tasks" 
+            :key="task.id"
+            @click="task.isCompleted = !task.isCompleted "
+           >
+            <div class="round" >
+                <input 
+                    type="checkbox" 
+                    @click="task.isCompleted = !task.isCompleted"
+                    :checked="task.isCompleted" 
+                    id="checkbox" />
+                <label for="checkbox"></label>
+                
+            </div>
+
+            <div :class="{task, checked: task.isCompleted}" > {{ task.name }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import tasks from '../data/tasks'
+
+export default ({
+    name: "ManagerTask",
+    data() {
+        return {
+           tasks
+        }
+    },
+
+})
+
+</script>
+<style>
+.container {
+    margin-top: 20px;
+    display: grid;
+    grid-template-columns: auto 1fr;
+    align-items: stretch;
+   justify-content: stretch;
+    gap: 30px;
+    height: 100%;
+}
+.task {
+    padding-top: 4px;
+    font-size: 20px;
+    cursor: pointer;
+}
+.round {
+  position: relative;
+}
+
+.round label {
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 50%;
+  cursor: pointer;
+  height: 28px;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 28px;
+}
+
+.round label:after {
+  border: 2px solid #fff;
+  border-top: none;
+  border-right: none;
+  content: "";
+  height: 6px;
+  left: 7px;
+  opacity: 0;
+  position: absolute;
+  top: 8px;
+  transform: rotate(-45deg);
+  width: 12px;
+}
+
+.round input[type="checkbox"] {
+  visibility: hidden;
+}
+
+.round input[type="checkbox"]:checked + label {
+  background-color: #66bb6a;
+  border-color: #66bb6a;
+}
+
+.round input[type="checkbox"]:checked + label:after {
+  opacity: 1;
+}
+
+.checked {
+    text-decoration: line-through;
+}
+
+
+
+
+
+</style>
+
